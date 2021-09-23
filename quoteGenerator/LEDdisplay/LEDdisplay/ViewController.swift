241dc73e61c7e10536310822ee4147f07493d4e0
imhyejeong <83940751+boraGom7@users.noreply.github.com>
2021-09-23 18:02:46 +0900

    set Project

---
+++ quoteGenerator/LEDdisplay/LEDdisplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LEDdisplay
+//
+//  Created by mac on 2021/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
